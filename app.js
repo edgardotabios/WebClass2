0e9c6be076e7d5f4b8bbaf4779ed97bb0d73f1bf
edgardotabios <edgardotabios@gmail.com>
2025-04-10 16:47:20 +1200

    Update

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
 app.set('view engine','ejs');
+
+app.use('/public', express.static('public'));
+
 app.get('/', function (req, res){
-res.render("home");
+    res.render("home");
 });
+
+app.get('/auckland', function (req, res){
+    res.render("auckland");
+});
+
 app.listen(3000);
 console.log('Node app is running on port 3000');
+
